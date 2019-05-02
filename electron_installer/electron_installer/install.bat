dde34d1ac95a2dacee4a53b3c058f99fb58f82ed
tsumikiHUANG <s2s4s6s8s2s4s6s8@gmail.com>
2019-05-02 12:25:09 +0800

    up

---
+++ electron_installer/electron_installer/install.bat
@@ -0,0 +1,6 @@
+call node-v10.15.3-x64.msi
+call VSCodeUserSetup-x64-1.33.1 /VERYSILENT
+
+call "C:\Program Files\nodejs\npm" install electron -g
+call "C:\Program Files\nodejs\npm" install electron-forge -g
+pause
